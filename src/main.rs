779c262893644cf786e028861f8a2d777491d674
Thinh Nguyen <thinhnguyenhuuhung@gmail.com>
2024-02-09 15:28:05 +0100

    [UPDATE] can't believe that I can print hello world

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,17 @@ use core::panic::PanicInfo;
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+
     loop {}
 }
 
+mod vga_buffer;
+
 #[no_mangle]
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start() {
+    println!("Hello World{}", "!");
+
     loop {}
 }
